5ed130357e9567cd034749aa9f0c858d493c1d98
Aditya Jain <67228475+Aditya2150@users.noreply.github.com>
2021-10-20 22:49:07 +0530

    Update A_Beautiful_Matrix.java
    Spy Number

--- A_Beautiful_Matrix.java
+++ A_Beautiful_Matrix.java
@@ -1,29 +1,32 @@
-import java.util.*;  
-public class NeonNumberExample1  
+import java.util.Scanner;  
+public class SpyNumberExample1   
 {  
 public static void main(String args[])  
-{  
-int sum = 0, n;      
+{   
+int num, product=1, sum=0, lastdigit;  
+// create object of scanner  
 Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the number to check: ");  
-//raeds an integer from the user  
-n = sc.nextInt();  
-//calculate square of the given number  
-int square = n * n;  
-//loop executes until the condition becomes false  
-while(square != 0)  
+System.out.print("Enter the number to check: " );  
+//reads an integer from the user and stores it in the variable num  
+num=sc.nextInt();  
+//executes untill the condition becomes false  
+while(num>0)  
 {  
-//find the last digit of the square      
-int digit = square % 10;  
-//adds digits to the variable sum  
-sum = sum + digit;  
-//removes the last digit of the variable square  
-square = square / 10;  
+//finds the last digit of the number      
+lastdigit=num%10;  
+//adds last digit to the variable sum  
+sum=sum+lastdigit;  
+//calculates the product  
+product=product*lastdigit;  
+//removes the last digit from the given number  
+num=num/10;  
 }  
-//compares the given number (n) with sum  
-if(n == sum)  
-System.out.println(n + " is a Neon Number.");  
+//compares the sum and product  
+if(sum==product)  
+//prints if the above condition returns true  
+System.out.println("The given number is a spy number.");  
 else  
-System.out.println(n + " is not a Neon Number.");  
+//prints if the above condition returns false  
+System.out.println("The given number is not a spy number.");  
 }  
 }  
